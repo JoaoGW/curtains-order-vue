b0cf22b9dc1d7ef0883ff9186c503236002f33b2
João Pedro do Carmo R <cpsenha@gmail.com>
2022-10-30 21:44:56 -0300

    Adicionado o cabecalho da página de cortinas com Bootstrap 5

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <header>
+    <CabecalhoFixo/>
+  </header>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+import CabecalhoFixo from './components/cabecalho.vue'
 
-export default defineComponent({
-  name: 'App',
+export default {
   components: {
-    HelloWorld
+    CabecalhoFixo //Importa o componente do cabecalho a página
   }
-});
+}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/cabecalho.vue
@@ -0,0 +1,46 @@
+<!--Cabecalho Feito com Bootstrap 5.2.0 padrão, não foram feitas muitas alterações por aqui a partir da versão padrão-->
+<template>
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../../public/index.html">Cortinas Premium</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../../public/index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Catalogo</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Orçamento
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Basic</a></li>
+                  <li><a class="dropdown-item" href="#">Regular</a></li>
+                  <li><a class="dropdown-item" href="#">Premium</a></li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Sobre</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'CabecalhoFixo' //Nome do Componente para exportação
+})
+</script>
